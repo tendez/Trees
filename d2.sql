2fdbc1ddf0caf0959c5d2c49371d37f1e24ba0e7
mikołaj <43722584+tendez@users.noreply.github.com>
2024-11-05 20:37:40 +0100

    Update project version, enhance accessibility, remove SQL query
    Updated Trees.csproj to version 1.2, added new version property, and set GeneratePackageOnBuild to False. Enhanced accessibility in LoginPage.xaml by adding AutomationId and SemanticProperties.Description to UsernameEntry and PasswordEntry. Removed a SQL query from d2.sql that joined multiple tables.

--- d2.sql
+++
@@ -1,7 +0,0 @@
-﻿
-                SELECT s.*, g.NazwaGatunku, w.OpisWielkosc, u.Login, st.StoiskoNazwa 
-                FROM Sprzedaz s
-                INNER JOIN Gatunek g ON s.GatunekID = g.GatunekID
-                INNER JOIN Wielkosc w ON s.WielkoscID = w.WielkoscID
-                INNER JOIN Uzytkownicy u ON s.UserID = u.UserID
-                INNER JOIN Stoisko st ON s.StoiskoID = st.StoiskoID
